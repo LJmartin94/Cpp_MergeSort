2a99d6fde6d82595001db237253bd73f31605135
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-04-15 16:24:06 +0200

    Accept generous donation of Ruben (Rvan-duy)'s parser.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
+
+std::vector<int> parseAndErrorCheckArgv(int argc, char **argv) 
+{
+  std::vector<int> numbers;
+  try {
+    for (int i = 1; i < argc; i++) {
+      std::stringstream ss(argv[i]);
+      long num;
+      while (ss >> num) {
+        if (ss.fail() || num < 0 || num > INT_MAX) {
+          throw std::invalid_argument("invalid argument");
+        }
+        numbers.push_back(num);
+      }
+    }
+  } catch (std::exception &e) {
+    std::cout << "Error: " << e.what() << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  return numbers;
+}
 
 int main(int argc, char **argv)
 {
@@ -7,5 +30,8 @@ int main(int argc, char **argv)
 		std::cout << "Not enough args!" << std::endl;
 		return (1);
 	}
+
+	std::vector<int> to_sort = parseAndErrorCheckArgv(argc, argv);
+
 	return (0);
 }
