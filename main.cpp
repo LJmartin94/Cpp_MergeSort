f49317fe09312b6ffa08f44c50fd9df5486212ad
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-05-04 18:06:51 +0200

    Is my git fucked?

--- main.cpp
+++ main.cpp
@@ -23,15 +23,74 @@ std::vector<int> parseAndErrorCheckArgv(int argc, char **argv)
   return numbers;
 }
 
-void ms_split(std::vector<int>& to_sort, std::vector<int>::iterator first, std::vector<int>::iterator last)
+void print_int_vector(std::vector<int> to_print, 
+					const std::vector<int>::iterator from,
+					const std::vector<int>::iterator to)
+{
+	for (std::vector<int>::iterator it = from; it != to; it++)
+		std::cout << *it;
+	std::cout << std::endl;
+}
+
+void swap_ints(std::vector<int>::iterator& a, std::vector<int>::iterator& b)
+{
+	int swap_holder = *a;
+	*a = *b;
+	*b = swap_holder;
+	return;
+}
+
+bool should_swap(int a, int b)
+{
+	if (a > b) //Or any other comparison.
+		return true;
+	return false;
+}
+
+void ms_merge(std::vector<int>& to_sort, 
+				std::vector<int>::iterator first, 
+				std::vector<int>::iterator mid, 
+				std::vector<int>::iterator last)
+{
+	std::cout << "ms_merge - segment: ";
+	std::vector<int> sorted;
+	print_int_vector(to_sort, first, last);
+	while( first < last && mid < last && first < mid)
+	{
+		std::cout << "Comparing " << first - first << " and " << mid - first << std::endl;
+		if (!should_swap(*first, *mid))
+		{
+			sorted.push_back(*first);
+			first++;
+		}
+		else
+		{
+			sorted.push_back(*mid);
+			mid++;
+		}
+	}
+	std::cout << "Storing result" << std::endl;
+	for (std::vector<int>::reverse_iterator rit = sorted.rbegin(); rit != sorted.rend(); ++rit)
+	{
+		last--;
+		*last = *rit;
+	}
+	return;
+}
+
+void ms_split(std::vector<int>& to_sort, 
+				std::vector<int>::iterator first, 
+				std::vector<int>::iterator last)
 {
 	std::vector<int>::iterator mid = ((last - first) / 2) + first;
-	std::cout << "Mid: " << mid - first << std::endl;
+	std::cout << "ms_split - Mid: " << mid - first << std::endl;
 	if ((last - first) > 1)
 	{
 		ms_split(to_sort, first, mid);
 		ms_split(to_sort, mid + 1, last);
 	}
+	if (first != mid)
+		ms_merge(to_sort, first, mid, last);
 	return;
 }
 
@@ -49,7 +108,7 @@ int main(int argc, char **argv)
 	ms_split(to_sort, to_sort.begin(), to_sort.end());
 
 	//Print answer
-	for (std::vector<int>::iterator it = to_sort.begin(); it != to_sort.end(); it++)
-		std::cout << *it << std::endl;
+	print_int_vector(to_sort, to_sort.begin(), to_sort.end());
+
 	return (0);
 }
